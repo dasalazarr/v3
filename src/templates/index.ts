c5cbc6ef99432433435abd595339db54e6e0d9fb
dasalazarr <diego_salazar@outlook.com>
2025-02-10 10:44:18 -0500

    primera prueba

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,17 +1,18 @@
 import { createFlow } from "@builderbot/bot";
 import { mainFlow } from "./mainflow";
-import { registerFlow } from "./registerFlow";
 import { faqFlow } from "./faqFlow";
 import { appointmentFlow, cancelAppointmentFlow } from "./appointmentFlow";
 import { contractFlow } from "./contractFlow";
 
 const flows = createFlow([
     mainFlow,
-    registerFlow,
     faqFlow,
     appointmentFlow,
     cancelAppointmentFlow,
     contractFlow
 ]);
 
-export default flows;
+export {
+    mainFlow,
+    faqFlow,
+};
--- src/templates/mainflow.ts
+++ src/templates/mainflow.ts
@@ -1,17 +1,9 @@
 import  { addKeyword, EVENTS } from "@builderbot/bot"
 import { faqFlow } from "./faqFlow"
-import sheetsServices from "../services/sheetsServices"
-import { registerFlow } from "./registerFlow";
 
 const mainFlow = addKeyword(EVENTS.WELCOME)
   .addAction(async (ctx, ctxFn) => {
-    const isUser = await sheetsServices.userExists(ctx.from);
-    if (!isUser) {
-        return ctxFn.gotoFlow(registerFlow);
-    }else{
-        ctxFn.gotoFlow(faqFlow)
-    } 
-
+    ctxFn.gotoFlow(faqFlow)
   });
 
 export { mainFlow };
