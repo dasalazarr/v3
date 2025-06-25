59e726568a0f2a21543e1a22d8ebbaa393503a1d
Diego Andres <dasalazarr@Diegos-MacBook-Pro.local>
2025-06-25 16:30:57 -0500

    fix: Register TranslationService in DI container

--- src/di/container.ts
+++ src/di/container.ts
@@ -4,6 +4,7 @@ import ScheduledTasks from '../services/scheduledTasks';
 import AIService from '../services/aiservices';
 import { AppointmentService } from '../services/appointments.service';
 import { AppointmentController } from '../services/appointments.controller';
+import { TranslationService } from '../services/translationService';
 
 // Registramos los servicios en el contenedor
 container.registerSingleton('SheetsService', SheetsService);
@@ -11,6 +12,7 @@ container.registerSingleton('ScheduledTasks', ScheduledTasks);
 container.registerSingleton('AIService', AIService);
 container.registerSingleton('AppointmentService', AppointmentService);
 container.registerSingleton('AppointmentController', AppointmentController);
+container.registerSingleton('TranslationService', TranslationService);
 
 // Exportamos el contenedor para uso en la aplicación
 export default container;
