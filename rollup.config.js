16141c08cdfd8cd6240efc45899a560eac4755c8
Diego Andrés Salazar <67563672+dasalazarr@users.noreply.github.com>
2025-07-12 21:08:07 -0500

    Move src to archive and update references

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from 'rollup-plugin-typescript2'
 
 export default {
-    input: 'src/app.ts',
+    input: 'archive/src/app.ts',
     output: {
         file: 'dist/app.js',
         format: 'esm',
